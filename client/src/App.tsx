34a3974c3fb275ce9ee9ffefafd4455a92656d6e
singwithaashish <newsingaasi@outlook.com>
2023-07-05 17:42:44 +0530

    first commit!

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'flowbite-react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Button color="primary">
+      <p className='text-green-600'>Hello World</p>
+    </Button>
+  )
+}
+
+export default App
